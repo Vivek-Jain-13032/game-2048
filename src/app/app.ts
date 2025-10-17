810394f8db3db5ea1bcbf95f4cea36e49d8d3f27
Vivek Jain <vivek.jain@intelliswift.com>
2025-10-17 14:18:06 +0530

    feat: add controls component and integrate keyboard event handling in game board

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { TileComponent } from "./components/tile/tile";
 import { GameBoard } from "./components/game-board/game-board";
+import { ControlsComponent } from "./components/controls/controls";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [TileComponent, GameBoard]
+  imports: [GameBoard, ControlsComponent]
 })
 export class App {
   protected title = 'game-2048';
--- src/app/components/game-board/game-board.ts
+++ src/app/components/game-board/game-board.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { GameService } from '../../service/game';
 import { TileComponent } from '../tile/tile';
 import { CommonModule } from '@angular/common';
@@ -15,6 +15,29 @@ export class GameBoard {
 
   readonly board = this.gameService.board;
   readonly boardSize = this.gameService.boardSize;
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent): void {
+    // Prevent default behavior for arrow keys
+    if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(event.key)) {
+      event.preventDefault();
+    }
+
+    switch (event.key) {
+      case 'ArrowUp':
+        this.gameService.move(Direction.UP);
+        break;
+      case 'ArrowDown':
+        this.gameService.move(Direction.DOWN);
+        break;
+      case 'ArrowLeft':
+        this.gameService.move(Direction.LEFT);
+        break;
+      case 'ArrowRight':
+        this.gameService.move(Direction.RIGHT);
+        break;
+    }
+  }
 
   trackByTileId(index: number, item: any): number {
     return item?.id ?? index;
